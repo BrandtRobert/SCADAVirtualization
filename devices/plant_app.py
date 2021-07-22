f295c5307984466d2255083c95662ccb80e8b304
Luis Rodriguez <treslar@rams.colostate.edu>
2021-07-22 13:35:18 -0600

    Update project Main with Modbus blocks+DB

--- devices/plant_app.py
+++ devices/plant_app.py
@@ -7,4 +7,76 @@ Created on Fri Jul 16 14:02:33 2021
 @company: Colorado State University
 """
 
-print("Hello World, I am the Plant")
+import time
+from labjack import ljm
+
+
+# ----- helpers
+
+def write(handle, pin="TDAC0", v=2.0):
+    return ljm.eWriteName(handle, pin, v)
+
+def read_analog(handle, pin="AIN0"):
+    return ljm.eReadName(handle,pin)
+
+def status_print(t, a, s1, s2, err):
+    print("\n"*16)
+    print("----------------------------------"
+              + "\n[{0}] Potentiometer value: {1}".format(t, a)
+              + "\n\t > Signal 1: -/+ 1  (TDAC0) = [{0}]".format(s1)
+              + "\n\t > Signal 2: offset (TDAC1) = [{0}]".format(s2)
+              + "\n err: {0}".format(err))
+
+
+# ----- main loop
+
+def main():
+    T7_id = "ANY"
+    T7_handle = ljm.openS("T7", "USB", T7_id)
+    T7_pins = ["TDAC0", "TDAC1"]
+    
+    signal1_v = 0
+    signal2_offset = 0
+    
+    
+    while True:
+        for e in range (1000):
+            # Alternate +/- 1 from base signal
+            if e % 10 == 0 and e != 0:
+                if signal1_v > signal2_offset:
+                    signal1_v = 0
+                else:
+                    signal1_v = 1
+            
+            # Change offset to 2
+            # > if pot is connected, this value will not matter
+            if e == 100:
+                if signal2_offset > 0:
+                    signal2_offset = 0
+                else:
+                    pot_input = read_analog(T7_handle)
+                    if pot_input < 1.0:
+                        signal2_offset = 2
+                    else:
+                        signal2_offset = int(pot_input)
+            
+            w_error_s1 = write(T7_handle, T7_pins[0], signal1_v)
+            _ = write(T7_handle, T7_pins[1], signal2_offset)
+            
+            ain0 = read_analog(T7_handle)
+            
+            # Print the registers for LJ and see if PLC replies
+            # > occurs every 10 seconds
+            # if e % 10 == 0:
+            #     status_print(e, ain0, signal1_v, signal2_offset, w_error_s1)
+            # else:
+            #     print("\r\n[{0}]".format(e))
+            
+            status_print(e, ain0, signal1_v, signal2_offset, w_error_s1)
+            time.sleep(1)
+
+if __name__ == '__main__':
+    
+    # Plant app
+    main()
+    
--- devices/scada_app.py
+++ devices/scada_app.py
@@ -7,4 +7,22 @@ Created on Fri Jul 16 13:38:26 2021
 @company: Colorado State University
 """
 
-print("Hello World, I am the SCADA app")
+import time
+from pyModbusTCP.client import ModbusClient
+
+
+print("Hello World, I am the SCADA app." 
+      +"\n[!] I will communicate to devices with Modbus")
+
+# TCP auto connect on first modbus request
+c = ModbusClient(host="192.168.0.241", port=502, unit_id=1)
+
+print("Attempting connection...")
+whi
+conn_status = c.open()
+
+print(conn_status)
+
+regs = c.read_holding_registers(0,2)
+
+print(regs)
