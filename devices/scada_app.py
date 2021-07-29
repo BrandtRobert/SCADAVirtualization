6f7e9cdb4431cc7de2baa89c0b5500d5f74bf7d4
Luis Rodriguez <treslar@rams.colostate.edu>
2021-07-29 11:36:50 -0600

    Added basic C203CPU code; updated MB code on SCADA app

--- devices/scada_app.py
+++ devices/scada_app.py
@@ -7,23 +7,65 @@ Created on Fri Jul 16 13:38:26 2021
 @company: Colorado State University
 """
 
-import time
+from time import sleep
+from random import uniform
+from pprint import pprint
+
 from pyModbusTCP.client import ModbusClient
 
+# globals
+SERVER_IP = "169.254.153.60"
+SERVER_PORT = 502
 
-print("Hello World, I am the SCADA app." 
+
+def scada_loop():
+    print("Hello World, I am the SCADA app." 
       +"\n[!] I will communicate to devices with Modbus")
 
-# TCP auto connect on first modbus request
-c = ModbusClient(host="192.168.0.241", port=502, unit_id=1)
+    
+    # TCP connect on first modbus request
+    client = ModbusClient(host=SERVER_IP, port=SERVER_PORT, unit_id=1)
+    
+    t = 0
+    
+    try:
+        print("Attempting connection...")    
+        if not client.is_open():
+            client.open()
+            
+        while client.is_open():
+            client.write_single_register(0, t)
+            regs = client.read_holding_registers(0,8)
+            t += 1
+            pprint(regs)
+            
+            sleep(1)
+            if t >= 100:
+                t = 0
+            
+    except:
+        print("Disconnected from device!\n")
+        sleep(2)
+        
+    print("Done.")
 
-
-while True:
-    print("Attempting connection...")
-    conn_status = c.open()
-    if conn_status:
-        print("[!] Connection established! Reading data...")
-        regs = c.read_holding_registers(0,2)
-        print(regs)
-
-
+if __name__=='__main__':
+    scada_loop()
+    # server = ModbusServer(SERVER_IP, SERVER_PORT, no_block=True)
+    
+    # try:
+    #     print("[?] Starting server...")
+    #     server.start()
+    #     print("[!] Server is online")
+    #     state = [0]
+        
+    #     while True:
+    #         DataBank.set_words(0, [int(uniform(0,100))])
+    #         if state != DataBank.get_words(1):
+    #             state = DataBank.get_words(1)
+    #             print(state)
+    #             sleep(0)
+        
+    # except:
+    #     print("[!] Server shutting down...")
+    #     server.stop()
