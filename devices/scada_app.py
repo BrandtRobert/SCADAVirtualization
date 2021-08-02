432a1758f6b8657129ab2be965318d16dee484a7
Luis Rodriguez <luisar@ETS00354.engr.ColoState.EDU>
2021-08-02 15:05:21 -0600

    Added Plant pulse LAD program to modbus_config

--- devices/scada_app.py
+++ devices/scada_app.py
@@ -21,22 +21,69 @@ SERVER_PORT = 502
    # Subnet Mask . . . . . . . . . . . : 255.255.254.0
    # Default Gateway . . . . . . . . . : 10.1.106.1
 
-def scada_loop():
-    print("Hello World, I am the SCADA app." 
-      +"\n[!] I will communicate to devices with Modbus")
+HORZ_LINE = "---------------------------"
 
+
+
+# MENU GUI
+def print_options():
+    print(HORZ_LINE)
+    print("1: Access Holding Registers")
+    
+    print("\n 0: exit SCADA system")
+    print(HORZ_LINE)
+
+def option_processing(opt, C):
+    if opt == 1:
+        print("[{0}] - Viewing Holding Reg data...".format(opt))
+        sleep(1)
+        access_h_regs(C)
+    else:
+        print("[{0}] - Nothing selected, exiting...".format(opt))
+        sleep(1)
+        return -1
+    
+
+# option 1: read Holding_Registers on MB-Server
+#   address range = 40001-50000 ; word(1 byte)
+def access_h_regs(C=ModbusClient):
+    regs = C.read_holding_registers(40001, 100)
+    print(regs)
+    #
+    # manipulate registers......
+    #
+    
+    
+
+# option 2: read Coils on MB-Server
+#   address range = 0-10000 ; bit
+def access_coils(C=ModbusClient):
+    coils = C.read_coils(0, 50)
+    print(coils)
+    
+
+def scada_loop():
+    print("~~~ SCADA App Starting ~~~")
     
     # Make a client/master
     client = ModbusClient(host=SERVER_IP, port=SERVER_PORT, unit_id=1)
-    
-    while True:
+    while not client.is_open():
             try:
                 print("[?] Attempting connection...")
-                status = client.open()
+                if client.open():
+                    print("\t Connected to server!")
             except:
                 print("[!] Disconnected from device with error!\n")
                 sleep(2)
-                    
+    
+    while client.is_open():
+        print_options()
+        option = input("Select an option:")
+        
+        status = option_processing(option, client)
+        
+        if status < 0:
+            break
         
     print("[.] Done.")
 
