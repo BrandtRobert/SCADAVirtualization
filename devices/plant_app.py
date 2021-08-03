fc19b9568f0b7a633cfeec76e313694eb71bde03
Luis Rodriguez <luisar@ETS00354.engr.ColoState.EDU>
2021-08-03 16:11:31 -0600

    Altered scada.py and plant.py; updated MB buffer to hold signal values

--- devices/plant_app.py
+++ devices/plant_app.py
@@ -7,11 +7,21 @@ Created on Fri Jul 16 14:02:33 2021
 @company: Colorado State University
 """
 
-import time
+from time import sleep
+from datetime import datetime
+from pprint import pprint
 from labjack import ljm
 
+# ----- globals
+
+time_series = {}
 
 # ----- helpers
+
+def get_current_time():
+    raw_time = datetime.now()
+    current_time = raw_time.strftime("%H:%M:%S")
+    return current_time
 
 def write(handle, pin="TDAC0", v=2.0):
     return ljm.eWriteName(handle, pin, v)
@@ -19,12 +29,12 @@ def write(handle, pin="TDAC0", v=2.0):
 def read_analog(handle, pin="AIN0"):
     return ljm.eReadName(handle,pin)
 
-def status_print(t, a, s1, s2, err):
-    print("\n"*16)
-    print("----------------------------------"
-              + "\n[{0}] Potentiometer value: {1}".format(t, a)
-              + "\n\t > Signal 1: -/+ 1  (TDAC0) = [{0}]".format(s1)
-              + "\n\t > Signal 2: offset (TDAC1) = [{0}]".format(s2)
+def status_print(t, pot_value, s1_value, s2_value, err):
+    print("\n"*4)
+    print("---- {0} ----".format(get_current_time())
+              + "\n[{0}] Potentiometer value: {1}".format(t, pot_value)
+              + "\n\t > Signal 1: -/+ 1  (TDAC0) = [{0}]".format(s1_value)
+              + "\n\t > Signal 2: offset (TDAC1) = [{0}]".format(s2_value)
               + "\n err: {0}".format(err))
 
 
@@ -38,45 +48,49 @@ def main():
     signal1_v = 0
     signal2_offset = 0
     
-    
-    while True:
-        for e in range (1000):
-            # Alternate +/- 1 from base signal
-            if e % 10 == 0 and e != 0:
-                if signal1_v > signal2_offset:
-                    signal1_v = 0
-                else:
-                    signal1_v = 1
-            
-            # Change offset to 2
-            # > if pot is connected, this value will not matter
-            if e == 100:
-                if signal2_offset > 0:
-                    signal2_offset = 0
-                else:
-                    pot_input = read_analog(T7_handle)
-                    if pot_input < 1.0:
-                        signal2_offset = 2
+    try:
+        while True:
+            for e in range (1000):
+                # Alternate +/- 1 from base signal
+                if e % 10 == 0 and e != 0:
+                    if signal1_v > signal2_offset:
+                        signal1_v = 0
                     else:
-                        signal2_offset = int(pot_input)
-            
-            w_error_s1 = write(T7_handle, T7_pins[0], signal1_v)
-            _ = write(T7_handle, T7_pins[1], signal2_offset)
-            
-            ain0 = read_analog(T7_handle)
-            
-            # Print the registers for LJ and see if PLC replies
-            # > occurs every 10 seconds
-            # if e % 10 == 0:
-            #     status_print(e, ain0, signal1_v, signal2_offset, w_error_s1)
-            # else:
-            #     print("\r\n[{0}]".format(e))
-            
-            status_print(e, ain0, signal1_v, signal2_offset, w_error_s1)
-            time.sleep(1)
+                        signal1_v = 1
+    
+                # Change offset to 2
+                # > if pot is connected, this value will not matter
+                if e == 100:
+                    if signal2_offset > 0:
+                        signal2_offset = 0
+                    else:
+                        pot_input = read_analog(T7_handle)
+                        if pot_input < 1.0:
+                            signal2_offset = 2
+                        else:
+                            signal2_offset = int(pot_input)
+                
+                w_error_s1 = write(T7_handle, T7_pins[0], signal1_v)
+                _ = write(T7_handle, T7_pins[1], signal2_offset)
+                
+                pot_ain = read_analog(T7_handle)
+                
+                # Print the registers for LJ and see if PLC replies
+                status_print(e, pot_ain, signal1_v, signal2_offset, w_error_s1)
+                
+                time_series[get_current_time()] = (signal1_v, signal2_offset)
+                
+                sleep(1)
+                
+    except KeyboardInterrupt:
+        print("Service terminated.")
+    
+    # process/output time series data into CSV
+    print(time_series)
 
 if __name__ == '__main__':
     
     # Plant app
     main()
+    
     
--- devices/scada_app.py
+++ devices/scada_app.py
@@ -8,11 +8,14 @@ Created on Fri Jul 16 13:38:26 2021
 """
 
 from time import sleep
+from datetime import datetime
 from pprint import pprint
 
+from threading import Thread, Lock
 from pyModbusTCP.client import ModbusClient
 
-# globals
+# ----- globals
+HORZ_LINE = "---------------------------"
 SERVER_IP = "10.1.106.200"
 SERVER_PORT = 502
 
@@ -21,15 +24,19 @@ SERVER_PORT = 502
    # Subnet Mask . . . . . . . . . . . : 255.255.254.0
    # Default Gateway . . . . . . . . . : 10.1.106.1
 
-HORZ_LINE = "---------------------------"
 
+# ----- helpers
 
+def get_current_time():
+    raw_time = datetime.now()
+    current_time = raw_time.strftime("%H:%M:%S")
+    return current_time
 
-# MENU GUI
+# --- Menu GUI
 def print_options():
     print(HORZ_LINE)
     print("1: Access Holding Registers")
-    
+    # future options added here
     print("\n 0: exit SCADA system")
     print(HORZ_LINE)
 
@@ -37,73 +44,60 @@ def option_processing(opt, C):
     if opt == 1:
         print("[{0}] - Viewing Holding Reg data...".format(opt))
         sleep(1)
-        access_h_regs(C)
+        return access_h_regs(C)
     else:
         print("[{0}] - Nothing selected, exiting...".format(opt))
         sleep(1)
         return -1
-    
 
 # option 1: read Holding_Registers on MB-Server
 #   address range = 40001-50000 ; word(1 byte)
 def access_h_regs(C=ModbusClient):
-    regs = C.read_holding_registers(40001, 100)
-    print(regs)
+    return -1
     #
     # manipulate registers......
     #
-    
-    
 
 # option 2: read Coils on MB-Server
 #   address range = 0-10000 ; bit
-def access_coils(C=ModbusClient):
+def access_coils(C):
     coils = C.read_coils(0, 50)
     print(coils)
-    
 
-def scada_loop():
+
+
+
+# ----- main loops
+# adapted from doc example:
+#   > https://pymodbustcp.readthedocs.io/en/latest/examples/modbus_thread.html
+def main():
     print("~~~ SCADA App Starting ~~~")
     
-    # Make a client/master
+        # Make a client/master
     client = ModbusClient(host=SERVER_IP, port=SERVER_PORT, unit_id=1)
-    while not client.is_open():
-            try:
-                print("[?] Attempting connection...")
-                if client.open():
-                    print("\t Connected to server!")
-            except:
-                print("[!] Disconnected from device with error!\n")
-                sleep(2)
     
-    while client.is_open():
-        print_options()
-        option = input("Select an option:")
-        
-        status = option_processing(option, client)
-        
-        if status < 0:
+    while True:
+        if not client.is_open():
+            client.open()
+            print("{0} > Connected to: {1}:{2}".format(
+                get_current_time(), SERVER_IP, SERVER_PORT))
             break
+    
+    # open TCP connection
+    for e in range(1000):
+        reg_list = client.read_holding_registers(0, 10)
         
-    print("[.] Done.")
+        if reg_list:
+            print("{0} > {1}".format(get_current_time(), reg_list()))
+        else:
+            print("{0} > ".format(get_current_time()))
+        sleep(1)
+        
+    print("Done.")
+
 
 if __name__=='__main__':
-    scada_loop()
-    # server = ModbusServer(SERVER_IP, SERVER_PORT, no_block=True)
     
-    # try:
-    #     print("[?] Starting server...")
-    #     server.start()
-    #     print("[!] Server is online")
-    #     state = [0]
-        
-    #     while True:
-    #         DataBank.set_words(0, [int(uniform(0,100))])
-    #         if state != DataBank.get_words(1):
-    #             state = DataBank.get_words(1)
-    #             print(state)
-    #             sleep(0)
-        
-    # except:
-    #     print("[!] Server shutting down...")
-    #     server.stop()
+    # SCADA app
+    main()
+    
