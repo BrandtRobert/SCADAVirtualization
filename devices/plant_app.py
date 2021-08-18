7f55ebb854a131152a9f83bddd7a36f631e48091
Luis Rodriguez <luisar@ETS00354.engr.ColoState.EDU>
2021-08-18 14:56:35 -0600

    Double signal code added to plant

--- devices/plant_app.py
+++ devices/plant_app.py
@@ -7,24 +7,37 @@ Created on Fri Jul 16 14:02:33 2021
 @company: Colorado State University
 """
 
+import csv
 from time import sleep
-from datetime import datetime
+from datetime import datetime as dt
 from pprint import pprint
 from labjack import ljm
+
+import sys
 
 # ----- globals
 
 time_series = {}
+signal_data = {}
 
 # ----- helpers
 
 def get_current_time():
-    raw_time = datetime.now()
+    raw_time = dt.now()
     current_time = raw_time.strftime("%H:%M:%S")
     return current_time
 
+def create_filetag():
+    t = dt.now().time()
+    seconds = (t.hour*60 + t.minute)*60 + t.second
+    return "{0}_{1}".format(dt.now().date(), seconds)
+
 def write(handle, pin="TDAC0", v=2.0):
     return ljm.eWriteName(handle, pin, v)
+
+def quick_write(pin="TDAC0", v=2.0):
+    temp_handle = ljm.openS("T7", "USB", "ANY")
+    return write(temp_handle, pin, v)
 
 def read_analog(handle, pin="AIN0"):
     return ljm.eReadName(handle,pin)
@@ -36,6 +49,12 @@ def status_print(t, pot_value, s1_value, s2_value, err):
               + "\n\t > Signal 1: -/+ 1  (TDAC0) = [{0}]".format(s1_value)
               + "\n\t > Signal 2: offset (TDAC1) = [{0}]".format(s2_value)
               + "\n err: {0}".format(err))
+
+def pins_state_print(handle, T):
+    X = []
+    for i in T:
+        X.append(read_analog(handle, i))
+    return X
 
 
 # ----- main loop
@@ -60,7 +79,7 @@ def main():
     
                 # Change offset to 2
                 # > if pot is connected, this value will not matter
-                if e == 100:
+                if e % 100 == 0 and e != 0:
                     if signal2_offset > 0:
                         signal2_offset = 0
                     else:
@@ -88,9 +107,81 @@ def main():
     # process/output time series data into CSV
     print(time_series)
 
+def double_signal():
+    # T7_id = "ANY"
+    T7_handle = ljm.openS("T7", "USB", "ANY")
+    T7_pins = ["TDAC0", "TDAC1"]
+    T7_ain_pins = ["AIN0", "AIN1"]
+    
+    # reset pins
+    write(T7_handle, T7_pins[0], 0)
+    write(T7_handle, T7_pins[1], 0)
+    
+    bool1 = False
+    bool2 = False
+    
+    print("Pins reset to 0v")
+    sleep(3)
+    
+    try:
+        for e in range(3000):
+            # fix signal 1
+            if bool1:
+                write(T7_handle, T7_pins[0], 1)
+            else:
+                write(T7_handle, T7_pins[0], 0)
+            bool1 = not bool1
+    
+            
+            # fix signal 2
+            if e % 10 == 0:
+                if bool2:
+                    write(T7_handle, T7_pins[1], 1)
+                else:
+                    write(T7_handle, T7_pins[1], 0)
+                bool2 = not bool2
+                
+            # print states
+            print("\n t:{0} > Pin states:".format(e))
+            pins = pins_state_print(T7_handle, T7_ain_pins)
+            print(pins)
+            
+            signal_data[e] = {"sequenceNum": e, "timestamp":get_current_time(),
+                             T7_ain_pins[0]: pins[0], T7_ain_pins[1]: pins[1]}
+            sleep(0.1)
+    
+    except KeyboardInterrupt:
+        print("Done. Data being saved.\n")
+    
+    
+    columns = signal_data[0].keys()
+    try:
+        with open("plant_data_{0}.csv".format(create_filetag()), "w") as csvfile:
+            print("Writing to {0}...".format(csvfile.name))
+            writer = csv.DictWriter(csvfile, fieldnames=columns)
+            writer.writeheader()
+            for d in signal_data:
+                writer.writerow(signal_data[d])
+    except IOError as err:
+        print("I/O error in CSV writing! ***\n\t{0}".format(err))
+    
+    print("Data saved.")
+    
+
 if __name__ == '__main__':
     
     # Plant app
-    main()
+    # main()
+    # double_signal()
+    quick_write()
+    quick_write(pin="TDAC1")
+    sleep(2)
+    quick_write(v=0)
+    quick_write(pin="TDAC1", v=0)
+    
+    
+    print("Done.")
+    
+
     
     
--- devices/scada_app.py
+++ devices/scada_app.py
@@ -16,7 +16,7 @@ from pyModbusTCP.client import ModbusClient
 
 # ----- globals
 HORZ_LINE = "---------------------------"
-SERVER_IP = "10.1.106.200"
+SERVER_IP = "10.1.106.202"
 SERVER_PORT = 502
 
    # Connection-specific DNS Suffix  . : research.colostate.edu
@@ -24,6 +24,7 @@ SERVER_PORT = 502
    # Subnet Mask . . . . . . . . . . . : 255.255.254.0
    # Default Gateway . . . . . . . . . : 10.1.106.1
 
+   # check for open IPs: "nmap -sP -PR 10.1.106.*"
 
 # ----- helpers
 
@@ -66,14 +67,13 @@ def access_coils(C):
 
 
 
-
 # ----- main loops
 # adapted from doc example:
 #   > https://pymodbustcp.readthedocs.io/en/latest/examples/modbus_thread.html
 def main():
     print("~~~ SCADA App Starting ~~~")
     
-        # Make a client/master
+    # make a client/master
     client = ModbusClient(host=SERVER_IP, port=SERVER_PORT, unit_id=1)
     
     while True:
@@ -82,18 +82,20 @@ def main():
             print("{0} > Connected to: {1}:{2}".format(
                 get_current_time(), SERVER_IP, SERVER_PORT))
             break
-    
-    # open TCP connection
-    for e in range(1000):
-        reg_list = client.read_holding_registers(0, 10)
-        
-        if reg_list:
-            print("{0} > {1}".format(get_current_time(), reg_list()))
-        else:
-            print("{0} > ".format(get_current_time()))
-        sleep(1)
-        
-    print("Done.")
+    try:
+        # open TCP connection
+        for e in range(1000):
+            client.write_single_register(40005, 50)
+            reg_list = client.read_holding_registers(40001, 8)
+            
+            # if reg_list:
+            print("\n{0} v\n{1}".format(get_current_time(), reg_list))
+            # else:
+            #     print("{0} > ".format(get_current_time()))
+            sleep(1)
+            
+    except KeyboardInterrupt:
+        print("Done.")
 
 
 if __name__=='__main__':
